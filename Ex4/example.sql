f66ac27f78f806f2ea08eaf4cd771b9076ff8119
Jannick <j.seper@stud.unibas.ch>
2025-10-03 12:55:49 +0200

    Adds same Strukture to the rest

---
+++ Ex4/example.sql
@@ -0,0 +1,11 @@
+-- Source: https://www.w3schools.com/sql/sql_create_table.asp
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+-- Source: https://www.w3schools.com/sql/sql_select.asp
+SELECT * FROM Persons;
